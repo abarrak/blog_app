1641ea54eed98838ade74d6ae2626d9c0aa96083
abarrak <a@abarrak.com>
2016-06-02 20:40:13 +0300

    add site structure integration testing

---
+++ test/integration/site_structure_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class SiteStructureTest < ActionDispatch::IntegrationTest
+  
+  test "Site layout and general structure" do
+    get root_path
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', root_path, count: 2
+
+    get contact_path
+    assert_match /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/, response.body
+  end
+end
